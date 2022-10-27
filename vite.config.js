61a848694b4a60a03c72b817d1b9e5c48ca95a33
Gaurav Sen <gaurav.s3n@gmail.com>
2022-10-27 16:07:00 +0530

    Build config added

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,25 @@
 import { defineConfig, loadEnv } from 'vite'
-
+import { resolve } from 'path'
 export default defineConfig(({ command, mode }) => {
     // Load env file based on `mode` in the current working directory.
     // Set the third parameter to '' to load all env regardless of the `VITE_` prefix.
     const env = loadEnv(mode, process.cwd(), '')
+    console.log(__dirname);
     return {
         // vite config
         define: {
             __APP_ENV__: env.APP_ENV
         },
-        root: "src"
+        root: "src",
+        build: {
+            outDir: "../dist",
+            rollupOptions: {
+                input: {
+                    main: resolve(__dirname, 'src/index.html'),
+                    dashboard: resolve(__dirname, 'src/dashboard/dashboard.html'),
+                    login: resolve(__dirname, 'src/login/login.html')
+                }
+            }
+        }
     }
 })
